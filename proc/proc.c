9bc23a6a46c9f9b94deb678b14d736f32329682e
AlwaysUnwilling <747266971@qq.com>
2021-09-11 10:21:05 +0800

    Proc

---
+++ proc/proc.c
@@ -0,0 +1,15 @@
+#include"proc.h"
+
+void proc()
+{
+    char bar[NUM];
+    memset(bar,'\0',sizeof(bar));
+    const char* lable="|/-\\";
+    int i=0;
+    while(i<100){
+        fflush(stdout);
+        bar[i++]='#';
+        printf("[%-100s][%3d%%][%c]\r",bar,i,lable[i%4]);
+        sleep(1);
+    }
+}
