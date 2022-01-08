07a4521d076c0b5d66953cacc63215549988c3ee
AlwaysUnwilling <747266971@qq.com>
2022-01-08 18:37:04 +0800

    1_8_Signal

---
+++ 1_8_Signal/signal.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<signal.h>
+
+
+
+void show_pending(sigset_t* pending)
+{
+    int sig=1;
+    for(;sig<=31;++sig){
+        if(sigismember(pending,sig)){
+            printf("1");
+        }else{
+            printf("0");
+        }
+    }
+    printf("\n");
+}
+void handler(int signum)
+{
+    printf("sig num is: %d\n",signum);
+}
+int main()
+{
+    signal(2,handler);
+    sigset_t pending;
+    sigset_t block,oblock;
+    sigemptyset(&block);
+    sigemptyset(&oblock);
+    sigaddset(&block,2);
+    sigprocmask(SIG_SETMASK,&block,&oblock);
+    int count=0;
+    while(1){
+        sigemptyset(&pending);
+        sigpending(&pending);
+        show_pending(&pending);
+        sleep(1);
+        count++;
+        if(count==20){
+            printf("recover sig mask!\n");
+            sigprocmask(SIG_SETMASK,&oblock,NULL);
+        }
+    }
+
+    return 0;
+}
