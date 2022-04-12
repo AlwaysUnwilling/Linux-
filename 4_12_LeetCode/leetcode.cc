5c002c84c3d6336773df8094a256a96304a0bd0a
AlwaysUnwilling <747266971@qq.com>
2022-04-12 09:39:30 +0800

    4_12_LeetCode

---
+++ 4_12_LeetCode/leetcode.cc
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<vector>
+#include<string>
+
+using namespace std;
+
+//806. 写字符串需要的行数
+/*
+ 我们要把给定的字符串 S 从左到右写到每一行上，每一行的最大宽度为100个单位，如果我们在写某个字母的时候会使这行超过了100 个单位，那么我们应该把这个字母写到下一行。我们给定了一个数组 widths ，这个数组 widths[0] 代表 'a' 需要的单位， widths[1] 代表 'b' 需要的单位，...， widths[25] 代表 'z' 需要的单位。
+
+现在回答两个问题：至少多少行能放下S，以及最后一行使用的宽度是多少个单位？将你的答案作为长度为2的整数列表返回。
+ * */
+class Solution {
+public:
+    vector<int> numberOfLines(vector<int>& widths, string s) {
+        vector<int> ret;
+        ret.resize(2);
+        if(s.size()==0){
+            return ret;
+        }
+        int end=0;
+        int line=1;
+        int length=0;
+        while(end<s.size()){
+            length+=widths[s[end]-'a'];
+            if(length>100){
+                line++;
+                length=widths[s[end]-'a'];
+            }else if(length==100){
+                if(end==s.size()-1){
+                    break;
+                }
+                line++;
+                length=0;
+            }
+            end++;
+        }
+        ret[0]=line;
+        ret[1]=length;
+        return ret;
+    }
+};
+
+
