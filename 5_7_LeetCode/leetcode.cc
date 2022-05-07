f335733cd187878e6031bbb7a558992ac2e24b7f
AlwaysUnwilling <747266971@qq.com>
2022-05-07 23:56:40 +0800

    5_7_LeetCode/

---
+++ 5_7_LeetCode/leetcode.cc
@@ -0,0 +1,71 @@
+#include<iostream>
+#include<stack>
+#include<limits.h>
+
+using namespace std;
+
+//98. 验证二叉搜索树
+/*
+ *给你一个二叉树的根节点 root ，判断其是否是一个有效的二叉搜索树。
+
+有效 二叉搜索树定义如下：
+
+节点的左子树只包含 小于 当前节点的数。
+节点的右子树只包含 大于 当前节点的数。
+所有左子树和右子树自身必须也是二叉搜索树。
+ * */
+
+//递归
+
+/**
+ * Definition for a binary tree node.*/ 
+ struct TreeNode {
+     int val;
+     TreeNode *left;
+     TreeNode *right;
+     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ };
+class Solution {
+public:
+    bool _isValidBST(TreeNode* root, long long lower, long long upper) {
+        if (root == nullptr) {
+            return true;
+        }
+        if (root -> val <= lower || root -> val >= upper) {
+            return false;
+        }
+        return _isValidBST(root -> left, lower, root -> val) && _isValidBST(root -> right, root -> val, upper);
+    }
+
+    bool isValidBST(TreeNode* root) {
+        return _isValidBST(root,LONG_MIN,LONG_MAX);
+    }
+};
+
+//方法二：中序遍历
+
+class Solution1 {
+public:
+    bool isValidBST(TreeNode* root) {
+        stack<TreeNode*> stack;
+        long long inorder = (long long)INT_MIN - 1;
+
+        while (!stack.empty() || root != nullptr) {
+            while (root != nullptr) {
+                stack.push(root);
+                root = root -> left;
+            }
+            root = stack.top();
+            stack.pop();
+            // 如果中序遍历得到的节点的值小于等于前一个 inorder，说明不是二叉搜索树
+            if (root -> val <= inorder) {
+                return false;
+            }
+            inorder = root -> val;
+            root = root -> right;
+        }
+        return true;
+    }
+};
