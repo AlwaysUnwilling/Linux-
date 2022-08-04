0499d39a5ca8670f6845d4732b9119f44dd5d68e
AlwaysUnwilling <747266971@qq.com>
2022-08-04 23:40:47 +0800

    8_4_LeetCode/

---
+++ 8_4_LeetCode/leetcode.cc
@@ -0,0 +1,102 @@
+#include<iostream>
+#include<string>
+#include<unordered_set>
+
+using namespace std;
+
+//208. 实现 Trie (前缀树)
+/*
+ *Trie（发音类似 "try"）或者说 前缀树 是一种树形数据结构，用于高效地存储和检索字符串数据集中的键。这一数据结构有相当多的应用情景，例如自动补完和拼写检查。
+
+请你实现 Trie 类：
+
+Trie() 初始化前缀树对象。
+void insert(String word) 向前缀树中插入字符串 word 。
+boolean search(String word) 如果字符串 word 在前缀树中，返回 true（即，在检索之前已经插入）；否则，返回 false 。
+boolean startsWith(String prefix) 如果之前已经插入的字符串 word 的前缀之一为 prefix ，返回 true ；否则，返回 false 。
+ * */
+
+//unordered_set 
+class Trie {
+public:
+    Trie() {
+
+    }
+    
+    void insert(string word) {
+        us.insert(word);
+    }
+    
+    bool search(string word) {
+        return us.find(word)!=us.end();
+    }
+    
+    bool startsWith(string prefix) {
+        int len1=prefix.size();
+        if(len1==0){
+            return false;
+        }
+        for(const string& str:us){
+            if(str[0]==prefix[0]){
+                string tmp=str.substr(0,len1);
+                if(tmp==prefix){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    unordered_set<string> us;
+};
+
+//字典树
+
+class Trie {
+private:
+    vector<Trie*> children;
+    bool isEnd;
+
+    Trie* searchPrefix(string prefix) {
+        Trie* node = this;
+        for (char ch : prefix) {
+            ch -= 'a';
+            if (node->children[ch] == nullptr) {
+                return nullptr;
+            }
+            node = node->children[ch];
+        }
+        return node;
+    }
+
+public:
+    Trie() : children(26), isEnd(false) {}
+
+    void insert(string word) {
+        Trie* node = this;
+        for (char ch : word) {
+            ch -= 'a';
+            if (node->children[ch] == nullptr) {
+                node->children[ch] = new Trie();
+            }
+            node = node->children[ch];
+        }
+        node->isEnd = true;
+    }
+
+    bool search(string word) {
+        Trie* node = this->searchPrefix(word);
+        return node != nullptr && node->isEnd;
+    }
+
+    bool startsWith(string prefix) {
+        return this->searchPrefix(prefix) != nullptr;
+    }
+};
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie* obj = new Trie();
+ * obj->insert(word);
+ * bool param_2 = obj->search(word);
+ * bool param_3 = obj->startsWith(prefix);
+ */
